112fff0ad5538ccb973024e8c9408c06e810a695
Kyle Corbin <kylecorbin26@gmail.com>
2019-10-30 17:21:51 -0400

    Finished the MVP

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,19 @@
+import React,{useEffect} from 'react';
+import {useLocalStorage} from "./useLocalStorage";
+
+const useDarkMode = () =>{
+    const [someValue, setSomeValue] = useLocalStorage('key', false)
+
+    useEffect(()=>{
+        if(someValue){
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode');
+        }
+    },[someValue])
+
+    
+    return [someValue, setSomeValue];
+}
+
+export default useDarkMode;
